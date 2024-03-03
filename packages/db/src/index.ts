c3cd13425c66d55adfb8e95d2a5806e9d140442f
Mateusz Kifner <kifner-mateusz@users.noreply.github.com>
2024-03-03 21:07:25 +0100

    refactor DB to use postgres

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,19 +1,15 @@
-import { Client } from "@planetscale/database";
 import { drizzle } from "drizzle-orm/planetscale-serverless";
+import postgres from "postgres";
 
 import * as auth from "./schema/auth";
 import * as post from "./schema/post";
 
 export const schema = { ...auth, ...post };
 
-export { mySqlTable as tableCreator } from "./schema/_table";
+export { pgTable as tableCreator } from "./schema/_table";
 
 export * from "drizzle-orm";
 
-const psClient = new Client({
-  host: process.env.DB_HOST!,
-  username: process.env.DB_USERNAME!,
-  password: process.env.DB_PASSWORD!,
-});
+const queryClient = postgres(process.env.DATABASE_URL!);
 
-export const db = drizzle(psClient, { schema });
+export const db = drizzle(queryClient, { schema });
--- packages/db/src/schema/auth.ts
+++ packages/db/src/schema/auth.ts
@@ -1,23 +1,22 @@
 import { relations, sql } from "drizzle-orm";
 import {
   index,
-  int,
+  integer,
   primaryKey,
   text,
   timestamp,
   varchar,
-} from "drizzle-orm/mysql-core";
+} from "drizzle-orm/pg-core";
 
-import { mySqlTable } from "./_table";
+import { pgTable } from "./_table";
 
-export const users = mySqlTable("user", {
+export const users = pgTable("user", {
   id: varchar("id", { length: 255 }).notNull().primaryKey(),
   name: varchar("name", { length: 255 }),
   email: varchar("email", { length: 255 }).notNull(),
   emailVerified: timestamp("emailVerified", {
     mode: "date",
-    fsp: 3,
-  }).default(sql`CURRENT_TIMESTAMP(3)`),
+  }),
   image: varchar("image", { length: 255 }),
 });
 
@@ -25,18 +24,18 @@ export const usersRelations = relations(users, ({ many }) => ({
   accounts: many(accounts),
 }));
 
-export const accounts = mySqlTable(
+export const accounts = pgTable(
   "account",
   {
     userId: varchar("userId", { length: 255 }).notNull(),
     type: varchar("type", { length: 255 })
-      .$type<"oauth" | "oidc" | "email">()
+      .$type<"oauth" | "oidc" | "email" | "credentials">()
       .notNull(),
     provider: varchar("provider", { length: 255 }).notNull(),
     providerAccountId: varchar("providerAccountId", { length: 255 }).notNull(),
     refresh_token: varchar("refresh_token", { length: 255 }),
     access_token: text("access_token"),
-    expires_at: int("expires_at"),
+    expires_at: integer("expires_at"),
     token_type: varchar("token_type", { length: 255 }),
     scope: varchar("scope", { length: 255 }),
     id_token: text("id_token"),
@@ -54,7 +53,7 @@ export const accountsRelations = relations(accounts, ({ one }) => ({
   user: one(users, { fields: [accounts.userId], references: [users.id] }),
 }));
 
-export const sessions = mySqlTable(
+export const sessions = pgTable(
   "session",
   {
     sessionToken: varchar("sessionToken", { length: 255 })
@@ -72,7 +71,7 @@ export const sessionsRelations = relations(sessions, ({ one }) => ({
   user: one(users, { fields: [sessions.userId], references: [users.id] }),
 }));
 
-export const verificationTokens = mySqlTable(
+export const verificationTokens = pgTable(
   "verificationToken",
   {
     identifier: varchar("identifier", { length: 255 }).notNull(),
